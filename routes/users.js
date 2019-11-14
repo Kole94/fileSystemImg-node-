d093d310432aa145ed285ab52161cdabc45aba82
Konstantin <konstantin.lijakovic@yahoo.com>
2019-11-14 19:52:21 +0100

    auth-user-pass

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var passport = require('passport');
+var User = require('../models/User');
+const bodyParser = require('body-parser');
+
+var router = express.Router();
+router.use(bodyParser.json());
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Registration Successful!'});
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'You are successfully logged in!'});
+});
+module.exports = router;
